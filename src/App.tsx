63457d2455100d1a7cd49e5c7840664f14ce4bf6
Hita K <rkambham@andrew.cmu.edu>
2021-06-03 16:19:28 -0400

    Added basic functionality of webpage and important objects (group, members, etc)

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //changes the preferences
+    changePreferences = (event: any) => {
+        // this.setState();
+    };
+
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Welcome to PURSE!
+            </p>
+            <input id="skilled" type="checkbox" onClick={this.changePreferences} />Require experience<br/>
+              <input type="text" name="enter" value="" id="numgroup"/> Number of Groups <br/>
+
+
+          </header>
+
+        </div>
+      );
+    }
 }
 
 export default App;
